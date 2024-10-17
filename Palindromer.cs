f5c2e43bd0d836c6412a67a620c9f586ca8bbca5
daniel178 <daniel@skylla.tech>
2024-10-17 10:52:20 -0400

    first commit

---
+++ Palindromer.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LCTesting
+{
+    /**
+    * Leetcode: Longest Palindrominc Substring
+    *
+    * given: a string s with len 1 <= s.Length <= 1000
+    * 
+    * find the longest palindrome within the substring.
+    * 
+    * e.g. LongestPalindrome("abba") = "abba"
+    * LongestPalindrome("gabba") = "abba"
+    * LongestPalindrome("geek") = "ee"
+    * LongestPalindrome("hi") = "h" or "i"
+    */
+    public class Palindromer
+    {
+        /*** The Naive Approach ***/
+        public static bool IsPalindrome(string s){
+            if (s.Length % 2 == 0) {
+                string s1 = s.Substring(0, s.Length / 2);
+                char[] s2_array = s.Substring(s.Length / 2, s.Length / 2).ToCharArray();
+                Array.Reverse(s2_array);
+                string s2 = new string(s2_array);
+                if (s1 == s2){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                string s1 = s.Substring(0, (s.Length)/2);
+                char[] s2_array = s.Substring((s.Length+1)/2, (s.Length)/2).ToCharArray();
+                Array.Reverse(s2_array);
+                string s2 = new string(s2_array);
+                if (s1 == s2){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        public static string LongestPalindrome(string s){
+            int c1 = 0;
+            string current_longest = s.Substring(c1, 1);
+            for (c1 = 0; c1 < s.Length; ++c1) {
+                int c2 = s.Length;
+                if (c2 - c1 <= current_longest.Length) {
+                    continue;
+                }
+                for (c2 = s.Length; c2 > c1; --c2) {
+                    string s2 = s.Substring(c1, c2 - c1);
+                    if (s2.Length > current_longest.Length && IsPalindrome(s2)) {
+                        current_longest = s2;
+                        break;
+                    }
+                }
+            }
+            return current_longest;            
+        }
+
+
+        /**** 2D array approach
+        from: https://www.geeksforgeeks.org/longest-palindromic-substring/#using-dynamic-programming
+         ****/
+        public static string LongestPalindrome2D(string s) {
+            int len = s.Length;
+            bool [,] arr = new bool[len, len];
+
+            // s[i] == s[i], so make all them true
+            for (int i = 0; i < len; ++i) {
+                arr[i, i] = true;
+            }
+
+            int maxLen = 1;
+            int start = 0;
+            // Check s[i] == s[i+1]
+            for (int i = 0; i < len - 1; ++i) {
+                if (s[i] == s[i+1]){
+                    arr[i, i+1] = true;
+                    start = i;
+                    maxLen = 2;
+                }
+            }
+            
+            // Check lengths greater than 2
+            for (int k = 3; k <= len; ++k) {
+                for(int i = 0; i < len - k + 1; ++i) {
+                    int j = i + k - 1;
+                    if (arr[i+1, j-1] && s[i] == s[j]) {
+                        arr[i, j] = true;
+                        if (k > maxLen) {
+                            start = i;
+                            maxLen = k;
+                        }
+                    }
+                }
+            }
+
+            return s.Substring(start, maxLen);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace LCTesting {
+    public class Program {
+        static void Main(string[] args) {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            // int[] nums1 = [1, 2];
+            // int[] nums2 = [3, 4];
+            // int[] nums3 = nums1.Concat(nums2).ToArray(); // Can also be  = [.. nums1, .. nums2];
+            // stopWatch.Stop();
+            // Console.WriteLine($"Time so far: {stopWatch.Elapsed}");
+            
+            // stopWatch.Restart();
+            // int[] nums4 = [.. nums1, .. nums2];
+            // stopWatch.Stop();
+            // Console.WriteLine($"Time for that other one: {stopWatch.Elapsed}");
+
+            // // We have determined list comprehensions are better in terms of speed.
+            // stopWatch.Restart();
+            // var nums5 = nums4.ToList<int>();
+            // nums5.Sort();
+            // stopWatch.Stop();
+            // Console.WriteLine($"To sort: {stopWatch.Elapsed}");
+
+            // foreach (int num in nums5) {
+            //     Console.Write($" {num}");
+            // }
+            // Console.WriteLine(" ");
+            
+            // it's still faster to use list comp 
+            // AND sort than to just do the concat opertation and get into an array.
+
+            Console.WriteLine("");
+
+            // Palindromer problem from leetcode
+            Palindromer p = new();
+            Console.WriteLine("PALINDROMER:");
+            // Console.WriteLine(p.LongestPalindrome("babad"));
+            // Console.WriteLine(p.LongestPalindrome("cbbd"));
+            stopWatch.Restart();
+            Console.WriteLine(Palindromer.LongestPalindrome("abba"));
+            Console.WriteLine(Palindromer.LongestPalindrome("gaabaag"));
+            Console.WriteLine(Palindromer.LongestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+            stopWatch.Stop();
+            Console.WriteLine($"Naive approach: {stopWatch.Elapsed}");
+            stopWatch.Restart();
+            Console.WriteLine(Palindromer.LongestPalindrome2D("abba"));
+            Console.WriteLine(Palindromer.LongestPalindrome2D("gaabaag"));
+            Console.WriteLine(Palindromer.LongestPalindrome2D("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+            stopWatch.Stop();
+            Console.WriteLine($"2D approach: {stopWatch.Elapsed}");
+        }
+    }
+}
